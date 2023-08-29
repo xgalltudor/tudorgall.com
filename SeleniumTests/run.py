310233737c2e00d8ee47c2b8e30b1dc034267cc9
Gall <RO100209@main.intgin.net>
2023-08-29 10:28:34 +0300

    Fix path for run.py

--- SeleniumTests/run.py
+++ SeleniumTests/run.py
@@ -1,9 +1,16 @@
 import subprocess
+import os
 
 
 def run_tests():
+    # Get the directory where the script resides
+    script_dir = os.path.dirname(__file__)
+
+    # Create the full path to the test_suites directory
+    test_suites_dir = os.path.join(script_dir, 'test_suites')
+
     # Run pytest on the test_suites directory with verbosity level 2
-    subprocess.run(['pytest', '-vv', './test_suites'])
+    subprocess.run(['pytest', '-vv', test_suites_dir])
 
 
 if __name__ == '__main__':
