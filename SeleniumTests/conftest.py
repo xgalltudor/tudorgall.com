e37a0884219b676b4e1923307cf63a815a5bfe9e
xgalltudor <63897239+xgalltudor@users.noreply.github.com>
2023-08-31 08:33:49 +0300

    Update conftest.py

--- SeleniumTests/conftest.py
+++ SeleniumTests/conftest.py
@@ -5,13 +5,16 @@ from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from test_resources.test_keywords import Keywords
 
+
 # Setup logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 @pytest.fixture(scope="module")
 def setup_teardown():
-    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+    service = Service(executable_path='/usr/local/bin/chromedriver')
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
     driver.implicitly_wait(5)
     keywords = Keywords(driver, logger)
     keywords.open_site()
