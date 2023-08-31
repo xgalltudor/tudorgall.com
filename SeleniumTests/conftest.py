8ecf24534cc142add5ca6d3aa101339da7cd0489
Gall <RO100209@main.intgin.net>
2023-08-31 11:10:17 +0300

    Add screenshot function

--- SeleniumTests/conftest.py
+++ SeleniumTests/conftest.py
@@ -1,19 +1,19 @@
 import pytest
 import logging
-import time
+import os
 from datetime import datetime
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from test_resources.test_keywords import Keywords
-
 
 # Setup logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-@pytest.fixture(scope="module")
-def setup_teardown():
+@pytest.fixture(scope="function")
+def setup_teardown(request):
     service = Service(executable_path='/usr/local/bin/chromedriver')
     options = webdriver.ChromeOptions()
     options.binary_location = "/usr/bin/google-chrome-stable"
@@ -28,4 +28,23 @@ def setup_teardown():
 
     yield keywords
 
+    if request.node.rep_call.failed:
+        take_screenshot(driver, request.node.name)
+
     driver.quit()
+
+def take_screenshot(driver, testcase):
+    now = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    file = os.path.join(os.environ.get('PYTHONPATH', '.'), f'screenshot_{testcase}_{now}.png')
+    try:
+        driver.get_screenshot_as_file(file)
+        logger.info(f"\n\nScreenshot taken: {file}\n\n")
+    except WebDriverException as e:
+        logger.info(f'Could not take a screenshot because of exception: {e}')
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
