a3ef31ae3c0676a995fdb30810af977919dc134e
Gall <RO100209@main.intgin.net>
2023-08-31 09:58:49 +0300

    Add screenshot and html report

--- SeleniumTests/conftest.py
+++ SeleniumTests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 import logging
+import time
+from datetime import datetime
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
@@ -27,3 +29,39 @@ def setup_teardown():
     yield keywords
 
     driver.quit()
+
+
+
+# set up a hook to be able to check if a test has failed
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
+
+
+# check if a test has failed
+@pytest.fixture(scope="function", autouse=True)
+def test_failed_check(request):
+    yield
+    # request.node is an "item" because we use the default
+    # "function" scope
+    if request.node.rep_setup.failed:
+        print("setting up a test failed!", request.node.nodeid)
+    elif request.node.rep_setup.passed:
+        if request.node.rep_call.failed:
+            driver = request.node.funcargs['selenium_driver']
+            take_screenshot(driver, request.node.nodeid)
+            print("executing test failed", request.node.nodeid)
+
+
+# make a screenshot with a name of the test, date and time
+def take_screenshot(driver, nodeid):
+    time.sleep(1)
+    file_name = f'{nodeid}_{datetime.today().strftime("%Y-%m-%d_%H:%M")}.png'.replace("/", "_").replace("::", "__")
+    driver.save_screenshot(file_name)
