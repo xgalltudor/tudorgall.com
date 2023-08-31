241ad817c782172932c42be60ee357eb5bc40ac8
Gall <RO100209@main.intgin.net>
2023-08-31 09:50:33 +0300

    Try a different approach for href

--- SeleniumTests/test_resources/test_keywords.py
+++ SeleniumTests/test_resources/test_keywords.py
@@ -179,7 +179,8 @@ class Keywords:
     def verify_pdf_cv(self, expected_url=Locators.cv_pdf_url):
         """Verifies that a PDF has the correct link when the user clicks the 'Download my PDF Curriculum Vitae' link."""
 
-        element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, Locators.download_cv_xpath)))
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
+            (By.XPATH, Locators.download_cv_xpath)))
         actual_url = element.get_attribute('href')
         print("Actual URL: ", actual_url)
         print("Expected URL: ", expected_url)
