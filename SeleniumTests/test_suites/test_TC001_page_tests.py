bc7a7cc9e6208c083a41fb378273ab9596314b67
xgalltudor <63897239+xgalltudor@users.noreply.github.com>
2023-08-31 08:31:15 +0300

    Update test_TC001_page_tests.py

--- SeleniumTests/test_suites/test_TC001_page_tests.py
+++ SeleniumTests/test_suites/test_TC001_page_tests.py
@@ -3,21 +3,6 @@ from selenium import webdriver
 import logging
 from test_resources.test_keywords import Keywords
 
-# Setup logging
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
-
-@pytest.fixture(scope="module")
-def setup_teardown():
-    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
-    keywords = Keywords(driver, logger)
-    keywords.open_site()
-
-    yield keywords  # Provide fixture value
-
-    driver.quit()
-
 
 def test_navigate_directly_to_home_page(setup_teardown):
     keywords = setup_teardown
