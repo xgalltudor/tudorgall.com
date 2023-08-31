43aaa18450b7d9fe1468e13decc906c56b14b7fe
xgalltudor <63897239+xgalltudor@users.noreply.github.com>
2023-08-31 09:06:54 +0300

    Update conftest.py

--- SeleniumTests/conftest.py
+++ SeleniumTests/conftest.py
@@ -16,6 +16,8 @@ def setup_teardown():
     options = webdriver.ChromeOptions()
     options.binary_location = "/usr/bin/google-chrome-stable"
     options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=service, options=options)
     driver.implicitly_wait(5)
