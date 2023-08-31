47fc40b8ad9adc0bb819e75624ef9cfad35ac753
xgalltudor <63897239+xgalltudor@users.noreply.github.com>
2023-08-31 08:03:41 +0300

    Update conftest.py

--- SeleniumTests/conftest.py
+++ SeleniumTests/conftest.py
@@ -3,25 +3,12 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 
+import pytest
+from selenium import webdriver
 
-@pytest.fixture()
-def setup(request):
-
-    chrome_options = Options()
-    options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-    for option in options:
-        chrome_options.add_argument(option)
-
-    request.cls.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-
-
-    yield request.cls.driver
-    request.cls.driver.close()
+@pytest.fixture(scope='function')
+def setup():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
