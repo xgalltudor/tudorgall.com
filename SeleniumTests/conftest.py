8068902fd0e658b8de6a4ab3e569d2a96a0e11ad
xgalltudor <63897239+xgalltudor@users.noreply.github.com>
2023-08-31 08:30:50 +0300

    Update conftest.py

--- SeleniumTests/conftest.py
+++ SeleniumTests/conftest.py
@@ -1,14 +1,21 @@
 import pytest
+import logging
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from test_resources.test_keywords import Keywords
 
-import pytest
-from selenium import webdriver
+# Setup logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
-@pytest.fixture(scope='function')
-def setup():
+@pytest.fixture(scope="module")
+def setup_teardown():
     driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
     driver.implicitly_wait(5)
-    yield driver
+    keywords = Keywords(driver, logger)
+    keywords.open_site()
+
+    yield keywords
+
     driver.quit()
